2ba31a4b68efdfb5fb90e16e144d604f10811736
Mark Hackl <hackl.mark@gmail.com>
2020-06-13 10:58:49 +0200

    Page Factories - Implementing Business classes Part 4

---
+++ src/test/java/com/w2a/testcases/FlightSearchTest.java
@@ -0,0 +1,18 @@
+package com.w2a.testcases;
+
+import com.w2a.base.Page;
+import com.w2a.pages.actions.HomePage;
+import org.testng.annotations.Test;
+
+public class FlightSearchTest {
+
+
+    @Test
+    public void FlightSearchTest() {
+
+        Page.initConfiguration();
+        HomePage home = new HomePage();
+        home.gotoFlights().bookAFlight("Vienna", "Berlin", "13/06/2020", "20/06/2020");
+        //Page.quitBrowser();
+    }
+}
---
+++ src/test/java/com/w2a/testcases/signInTest.java
@@ -0,0 +1,16 @@
+package com.w2a.testcases;
+
+import com.w2a.base.Page;
+import com.w2a.pages.actions.SigninPage;
+import org.testng.annotations.Test;
+
+public class signInTest {
+
+    @Test
+    public void signInTest() throws InterruptedException {
+        Page.initConfiguration();
+        SigninPage signinPage = Page.topNav.gotoSignIn();
+        Thread.sleep(2000);
+        signinPage.doLogin("test.automatisierer@gmail.com", "Autotest@123");
+    }
+}
