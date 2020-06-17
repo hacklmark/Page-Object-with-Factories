3fdfe8dc4825a1d2878dde5dc774ea2eeab3e329
Mark Hackl <hackl.mark@gmail.com>
2020-06-17 12:26:53 +0200

    Page Factories - Adding Keywords and Implementing logs

--- src/test/java/com/w2a/testcases/FlightSearchTest.java
+++ src/test/java/com/w2a/testcases/FlightSearchTest.java
@@ -4,19 +4,28 @@ import com.w2a.base.Page;
 import com.w2a.errorcollectors.ErrorCollector;
 import com.w2a.pages.actions.HomePage;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FlightSearchTest {
 
+    @BeforeTest
+    public void setUp(){
+        Page.initConfiguration();
+    }
 
     @Test
     public void FlightSearchTest() throws InterruptedException {
 
-        Page.initConfiguration();
         HomePage home = new HomePage();
         //Assert.assertEquals(home.findTabCount(), 5);
         ErrorCollector.verifyEquals(home.findTabCount(), 5);
         home.gotoFlights().bookAFlight("Vienna", "Berlin", "13/06/2020", "20/06/2020");
+    }
+
+    @AfterTest
+    public void tearDown(){
         Page.quitBrowser();
     }
 }
--- src/test/java/com/w2a/testcases/signInTest.java
+++ src/test/java/com/w2a/testcases/signInTest.java
@@ -2,16 +2,26 @@ package com.w2a.testcases;
 
 import com.w2a.base.Page;
 import com.w2a.pages.actions.SigninPage;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class signInTest {
 
+    @BeforeTest
+    public void setUp(){
+        Page.initConfiguration();
+    }
+
     @Test
     public void signInTest() throws InterruptedException {
-        Page.initConfiguration();
         SigninPage signinPage = Page.topNav.gotoSignIn();
         Thread.sleep(2000);
         signinPage.doLogin("test.automatisierer@gmail.com", "Autotest@123");
+    }
+
+    @AfterTest
+    public void tearDown(){
         Page.quitBrowser();
     }
 }
